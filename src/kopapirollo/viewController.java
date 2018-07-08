4d4617e3ec49d635ee1083faad629fa695f18c1a
dencs <dencs.janos@gmail.com>
2018-07-08 15:48:36 +0200

    kész

--- src/kopapirollo/viewController.java
+++ src/kopapirollo/viewController.java
@@ -12,6 +12,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 
 import javafx.scene.image.ImageView;
+import javafx.scene.text.Text;
 
 /**
  *
@@ -19,7 +20,14 @@ import javafx.scene.image.ImageView;
  */
 public class viewController implements Initializable {
     
-//<editor-fold defaultstate="collapsed" desc="comment">
+    //számon tartja hogy éppen ki hányat nyert
+    private int winPc = 0;
+    private int winPlayer = 0;
+    //az aktuális mutatás kerül bele (kő, papír vagy olló
+    private String toolPc = "";
+    private String toolPlayer = "";
+    
+//<editor-fold defaultstate="collapsed" desc="FXML">
     @FXML
     private ImageView imgRockPc;
     
@@ -46,24 +54,52 @@ public class viewController implements Initializable {
     
     @FXML
     private Button btnScissors;
-//</editor-fold>
     
+    @FXML
+    private Text txtPayerScore;
+    
+    @FXML
+    private Text txtPcScore;
+//</editor-fold>
+    //ha az ollóra kattint a játékos...
     @FXML
     private void handlerScissorsButtonClick(){
-        mutasd(imgScissorsPlayer);
+        change(imgScissorsPlayer);
     }
-    
+    //ha az papírra kattinta játékos...
     @FXML
     private void handlerPaperButtonClick(){
-        mutasd(imgPaperPlayer);
+        change(imgPaperPlayer);
     }
-    
+    //ha az kőre kattint a játékos...
     @FXML
     private void handlerRockButtonClick(){
-        mutasd(imgRockPlayer);
+        change(imgRockPlayer);
     }
     
-    private void mutasd(ImageView showImg){
+    //újraindul a játék minden értéket alapra állítok
+    @FXML
+    private void handlerRestartButtonClick(){
+        imgRockPlayer.setVisible(false);
+        imgScissorsPlayer.setVisible(false);
+        imgPaperPlayer.setVisible(false);
+        
+        imgRockPc.setVisible(false);
+        imgScissorsPc.setVisible(false);
+        imgPaperPc.setVisible(false);
+        
+        winPc = 0;
+        winPlayer = 0;
+    
+        toolPc = "";
+        toolPlayer = "";
+        
+        txtPcScore.setText("0");
+         txtPayerScore.setText("0");
+    }
+    
+    
+    private void change(ImageView showImg){
         imgRockPlayer.setVisible(false);
         imgScissorsPlayer.setVisible(false);
         imgPaperPlayer.setVisible(false);
@@ -73,19 +109,40 @@ public class viewController implements Initializable {
         imgPaperPc.setVisible(false);
         
         showImg.setVisible(true);
+        toolPlayer = showImg.getId();
         
+        // random szám generálással oldottam meg a gép mutatását (1: kő, 2: papír, 3: olló)
         int random = (int) (Math.random()*3)+1;
         System.out.println(String.valueOf(random));
         switch(random){
             case 1: imgRockPc.setVisible(true);
+                    toolPc = imgRockPc.getId();
             break;
             case 2: imgPaperPc.setVisible(true);
+                    toolPc = imgPaperPc.getId();
             break;
             case 3: imgScissorsPc.setVisible(true);
+                    toolPc = imgScissorsPc.getId();
+            break;
         }
+        //System.out.println(toolPc + " - " + toolPlayer);
+        
+        //aki nyer annak a pontszámát felplusszolom és beleteszem a megfelelő text mezőbe
+        if ((toolPc.equals("imgScissorsPc") && toolPlayer.equals("imgPaperPlayer"))
+                ||  (toolPc.equals("imgPaperPc") && toolPlayer.equals("imgRockPlayer"))
+                ||  (toolPc.equals("imgRockPc") && toolPlayer.equals("imgScissorsPlayer"))){
+            txtPcScore.setText(String.valueOf(++winPc));
+        }else if((toolPc.equals("imgScissorsPc") && toolPlayer.equals("imgScissorsPlayer"))
+                || (toolPc.equals("imgPaperPc") && toolPlayer.equals("imgPaperPlayer"))
+                || (toolPc.equals("imgRockPc") && toolPlayer.equals("imgRockPlayer"))){
+            
+        }else{
+            txtPayerScore.setText(String.valueOf(++winPlayer));
+        }
+       // System.out.println("" + winPc + " " + winPlayer);
     }
     
-    
+    //kezdéskor elrejtek minden képet, kinullázom a pontokat mutató text-eket
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         imgRockPc.setVisible(false);
@@ -94,7 +151,11 @@ public class viewController implements Initializable {
         imgRockPlayer.setVisible(false);
         imgScissorsPlayer.setVisible(false);
         imgPaperPlayer.setVisible(false);
-        
+        txtPayerScore.setText("0");
+        txtPcScore.setText("0");
+        btnRock.setFocusTraversable(false);
+        btnPaper.setFocusTraversable(false);
+        btnScissors.setFocusTraversable(false);
     }
     
 }
